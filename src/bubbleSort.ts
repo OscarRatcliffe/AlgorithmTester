4ac2baddb7d10728938c76c4d68d728ea0013141
Oscar <cyberskely@gmail.com>
2023-10-03 10:42:56 +0100

    Written bubble sort implimentation

--- src/bubbleSort.ts
+++ src/bubbleSort.ts
@@ -1,3 +1,21 @@
-function bubbleSort(toSort: Array<number>) {
-    
+function bubbleSort(toSort: Array<number>): Array<number> {
+    var earlyExit: boolean = false;
+    while(!earlyExit) {
+
+        earlyExit = true;
+
+        for (var i = 0; i < toSort.length; i++) {
+            var num1: number = toSort[i];
+            var num2: number = toSort[i+1];
+
+            if (num1 > num2) {
+                toSort[i] = num2;
+                toSort[i+1] = num1;
+                earlyExit = false;
+            }
+
+            console.log(toSort)
+        }
+    }
+    return toSort
 }
