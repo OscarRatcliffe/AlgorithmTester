505c0940a303d0a1ed9b312e0d8c91ff6839b47b
Oscar <cyberskely@gmail.com>
2023-10-04 14:44:48 +0100

    reworked testing

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,17 +1,20 @@
 const {bubbleSort, insertionSort, main} = require('../build/bundle')
+const readline = require('readline-sync');
 
 // ------------------
 // Sorting algorithms 
 // ------------------
 
+const inputData = [92, 86, 58, 35, 13];
+const expectedOutput = [13, 35, 58, 86, 92]
+
 test("Bubble sort func", () => {
-    // expect(bubbleSort)([92, 86, 58, 35, 13])).toBe([13, 35, 58, 86, 92];
-    expect(bubbleSort([92, 86, 58, 35, 13])).toEqual([13, 35, 58, 86, 92])
+    expect(bubbleSort(inputData)).toEqual(expectedOutput)
 });
 
+
 test("Insertion sort func", () => {
-    // expect(bubbleSort)([92, 86, 58, 35, 13])).toBe([13, 35, 58, 86, 92];
-    expect(insertionSort([92, 86, 58, 35, 13])).toEqual([13, 35, 58, 86, 92])
+    expect(insertionSort(inputData)).toEqual(expectedOutput)
 });
 
 
@@ -19,3 +22,22 @@ test("Insertion sort func", () => {
 // Whole program behaviour
 // -----------------------
 
+jest.mock('readline-sync');
+
+test("Valid bubble test", () => {
+
+    beforeEach(() => {
+        jest.spyOn(readline, 'question');
+      });
+    
+      afterEach(() => {
+        jest.restoreAllMocks();
+      });
+
+    readline.question
+        .mockReturnValueOnce("5")
+        .mockReturnValueOnce("B")
+        .mockReturnValueOnce("Y");
+
+    expect(main).toBe(1);
+});
